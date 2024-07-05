9e642f1a2a8883e6d891386aefdaab8ce18b875c
hxwilliam <hxwilliam@skiff.com>
2024-07-05 15:34:27 +0200

    no jekyll pls

--- dist/hx-qr-codes/server/main.server.mjs
+++ dist/hx-qr-codes/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3PQSYBBO.mjs";import"./chunk-AR6C2GFU.mjs";import"./chunk-ILYM5BAW.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-JL2RDPOU.mjs";import"./chunk-AR6C2GFU.mjs";import"./chunk-ILYM5BAW.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
